c2b09ac253a6b1206dbb8b8c039761c1b1b8c52d
galeczka <56445236+galeczka@users.noreply.github.com>
2021-06-18 12:40:30 +0200

    board utils

--- app/src/main/java/mobilne/projekt/minesweeper/game/moves/ChordMove.kt
+++ app/src/main/java/mobilne/projekt/minesweeper/game/moves/ChordMove.kt
@@ -3,6 +3,7 @@ package mobilne.projekt.minesweeper.game.moves
 import mobilne.projekt.minesweeper.forEachEightNeighbor
 import mobilne.projekt.minesweeper.game.Board
 
+
 class ChordMove(val row: Int, val column: Int) : Move {
     override fun execute(board: Board, changeSet: Board.ChangeSet) {
         if (!board.isRevealed(row, column)) return
