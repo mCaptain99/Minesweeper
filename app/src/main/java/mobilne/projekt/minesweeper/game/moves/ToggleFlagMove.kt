4ec9039b939942ca2b11cee7c41abcb0f0b4f7cc
galeczka <56445236+galeczka@users.noreply.github.com>
2021-06-18 12:41:24 +0200

    board utils

--- app/src/main/java/mobilne/projekt/minesweeper/game/moves/ToggleFlagMove.kt
+++ app/src/main/java/mobilne/projekt/minesweeper/game/moves/ToggleFlagMove.kt
@@ -3,6 +3,7 @@ package mobilne.projekt.minesweeper.game.moves
 import mobilne.projekt.minesweeper.game.Board
 import mobilne.projekt.minesweeper.game.moves.Move
 
+
 class ToggleFlagMove(val row: Int, val column: Int) : Move {
     override fun execute(board: Board, changeSet: Board.ChangeSet) {
         if (board.isFlagged(row, column) || board.isRevealed(row, column))
